94bdf16b91ab98331a453310ea86842bdddee4b4
Mariana Chiorboli <marianachiorboli@admdocket.com.br>
2025-06-18 14:30:45 -0300

    Entrada de dados + conversão de dados

--- input.py
+++ input.py
@@ -1,3 +1,19 @@
 #UTilizando o input(entrada de dado)
+#Código sem converter o tipo de dado, ele identifica tudo como String
+"""
+name = input("Digite o nome do filme: \n")
+yearLaunch = input("Digite o ano de lançamento do filme: \n")
+noteMovie = input("Informe a nota: \n")
 
-name = input("Digite o nome do filme: ")
+print (type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
+"""
+#Tipo de dado convertido
+name = input("Digite o nome do filme: \n")
+yearLaunch = int(input("Digite o ano de lançamento do filme: \n"))
+noteMovie = float(input("Informe a nota: \n"))
+
+print (type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
