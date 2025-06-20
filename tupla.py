0864bfd8640884865d1a5e61452a9e894ede2192
Mariana Chiorboli <marianachiorboli@admdocket.com.br>
2025-06-20 14:21:16 -0300

    Aprendendo tuplas

---
+++ tupla.py
@@ -0,0 +1,17 @@
+filmesTuple = ( "Branca de Neve", "Cinderela", "Frozen",
+              "Bela Adormecida", "Interstellar")
+
+# 1 - Buscar os dois primeiros itens da tupla
+print(filmesTuple[:2])
+
+# 2 - buscar o último item da tupla
+print(filmesTuple[-1])
+
+# 3 - Buscar filme até determinada posição 
+print(filmesTuple[:3])
+
+# 4 - Buscar filmes de uma posição em diante 
+print(filmesTuple[3:])
+
+# 5 - Recuperar um item da tupla pelo nome, trazer um indice de uma determinada posição
+print(filmesTuple.index("Branca de Neve"))
