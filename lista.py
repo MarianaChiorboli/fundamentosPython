7bd951669bd430d8d3fe4b6cd1e98feb11bfbb9b
Mariana Chiorboli <marianachiorboli@admdocket.com.br>
2025-06-20 12:01:04 -0300

    Aprendendo a manipular listas

---
+++ lista.py
@@ -0,0 +1,17 @@
+filmeMatrix = ["Matrix", 1999, 8.7, True]
+print(filmeMatrix)
+
+filmesList = ["Branca de Neve", "Cinderela", "Frozen",
+              "Bela Adormecida", "Interstellar"]
+
+#1 - Buscando os dois primeiros itens da lista 
+print(filmesList[:2])#2 - considera a posição zero e um 
+
+#2- buscar o último item da lista
+print(filmesList[-1])
+
+#3- Buscar filmes até uma determinada posição 
+print(filmesList[:3])
+
+#4- Busca filme de uma posição em diante 
+print(filmesList[1:3])
