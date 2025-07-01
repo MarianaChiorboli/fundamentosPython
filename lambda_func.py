70d26f48e7bbd6b10f89b72e83fbbe6ab3ceaf50
Mariana Chiorboli <marii.costa260531@gmail.com>
2025-07-01 18:42:18 -0300

    test: Função lambada

---
+++ lambda_func.py
@@ -0,0 +1,18 @@
+#Essa função é recebida uma váriavel
+# Função de potência de um número:
+
+power = lambda num:  num ** 2
+print(power(5))
+
+#Função que verifica se o número é par
+is_even = lambda x: x % 2 == 0
+print(is_even(6))
+
+#Função que divide um número por outro 
+divide = lambda x, y: x/y
+print(divide(8,4))
+
+# Função que inverte uma String 
+reverse = lambda s:s[::-1]
+print(reverse("MAri"))
+
